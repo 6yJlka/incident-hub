ce6b133f17b96bc5c9892207be192cea3b534d87
6yJlka <donskikh2004@mail.ru>
2026-07-28 17:27:54 +0300

    first commit

---
+++ src/test/java/ru/donskikh/incidenthub/IncidentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.donskikh.incidenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
